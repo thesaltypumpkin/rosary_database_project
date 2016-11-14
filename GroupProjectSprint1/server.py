a20cd60e43e5362c0ad19d34552e4db3cd587643
Max Taylor II <mtaylor8@mail.umw.edu>
2016-11-14 20:41:45 +0000

    minor changes to links

--- GroupProjectSprint1/server.py
+++ GroupProjectSprint1/server.py
@@ -3,16 +3,33 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
-
 @app.route('/')
 def mainIndex():
-
     return render_template('home.html')
 
 @app.route('/order')
-def order(): 
-    
+def order():
     return render_template('orderform.html')
+    
+@app.route('/login')
+def login():
+    return render_template('logIn.html')
+    
+@app.route('/about')
+def about():
+    return render_template('About.html')
+    
+@app.route('/premade')
+def premade():
+    return render_template('PremadeItems.html')
+    
+@app.route('/catalog')
+def catalog():
+    return render_template('catalog.html')
+    
+@app.route('/admin')
+def admin():
+    return render_template('Admin.html')
 
 # start the server
 if __name__ == '__main__':
